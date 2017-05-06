a41aa9a8a3c514629731d943154f67b325bead19
Gleb Tregubov <glebtregubov@Tregubov-Gleb.local>
2017-05-07 00:54:28 +0200

    lucky_ticket_result

--- Gleb_Tregubov/lesson-1/warfolder/advanced/lucky_ticket.js
+++ Gleb_Tregubov/lesson-1/warfolder/advanced/lucky_ticket.js
@@ -1,20 +1,21 @@
 var ticketNumber = 316613;
 var a1 = ticketNumber % 10;
-
+alert(a1);
 var a2 = Math.floor(ticketNumber % 100 / 10);
-
+alert(a2);
 var a3 = Math.floor(ticketNumber % 1000 / 100);
-
+alert(a3);
 var a4 = Math.floor(ticketNumber % 10000 / 1000);
-
+alert(a4);
 var a5 = Math.floor(ticketNumber % 100000 / 10000);
-
+alert(a5);
 var a6 = Math.floor(ticketNumber % 1000000 / 100000);
-
+alert(a6);
 
 var sum1 = a1 + a2 + a3;
-
+alert(sum1);
 var sum2 = a4 + a5 + a6;
+alert(sum2);
 
 var result = sum1 == sum2 ? 'Счастливый билет' : 'Несчастливый билет';
 alert(result);
