74dfb4ca3e7ce9fe5efcc3eb981ea1a6637e7435
Любовь <deese@list.ru>
2017-05-07 12:29:42 +0300

    Add files via upload

---
+++ Любовь_Морозова/lesson-1/homework/standard_homework/st.js
@@ -0,0 +1,14 @@
+var Tc = 23;
+var Tf = (9/5)*Tc+32;
+console.log( "Температура по Фаренгейту = " + Tf);
+
+
+var name;
+var admin;
+name="Василий";
+admin=name;
+console.log(admin);
+
+var i = 1000;
+var a = '108';
+// a+i выведет 1000108, т.к. сложение числового типа данных со строкой/
