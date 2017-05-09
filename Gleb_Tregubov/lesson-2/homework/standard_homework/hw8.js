e0553c96f071c5a2a30bd1c874620dc76f6e5bd0
Gleb Tregubov <glebtregubov@Tregubov-Gleb.local>
2017-05-09 21:48:45 +0200

    hw2_8

--- Gleb_Tregubov/lesson-2/homework/standard_homework/hw8.js
+++ Gleb_Tregubov/lesson-2/homework/standard_homework/hw8.js
@@ -1,3 +1,12 @@
 function power( val, pow ){
-    multip =
+
+    if (pow >= 2){
+        return val * power( val, (pow -1) );
+    }else if ( pow = 1 ){
+        return val;
+    }else if ( pow = 0 ){
+    		return 1;
+    }
+
 }
+console.log( power( 3, 3) );
