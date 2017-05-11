141dad9ba737cb56f3084675ad4cd7850b26f81d
Damir92 <home_damir92@mail.ru>
2017-05-11 16:31:42 +0300

    First from advanced

---
+++ Damir_Masalimov/lesson-3/homework/standard_homework/3withoutBody.js
@@ -0,0 +1,2 @@
+for ( var i = 1; i <= 9; i++, console.log(i)) {
+}
---
+++ Damir_Masalimov/lesson-3/homework/standard_homework/4Pyramid.js
@@ -0,0 +1,10 @@
+var n = 20,
+stroka;
+
+for ( var i = 1; i <= n; i++) {
+	stroka = '';
+	for ( var j = 1; j <= i; j++ ) {
+  stroka += 'x';
+  }
+  console.log( stroka );
+}
