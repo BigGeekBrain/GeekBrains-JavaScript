01d39171179b3543b2c49aca4bc42b3166cbd218
Damir92 <home_damir92@mail.ru>
2017-05-12 15:26:57 +0300

    Damir +

--- Group_projects/group_#1/project/gulpfile.js
+++ Group_projects/group_#1/project/gulpfile.js
@@ -1,28 +1,54 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    pug = require('gulp-pug'),
+    imagemin = require('gulp-imagemin');
 
 gulp.task('js:compress', function() {
-	function run() {
-		return gulp.src([
-			'js/*.js',
-			'!js/*.min.js',
-			'vendor/bootstrap/js/bootstrap.js',
-			'vendor/jquery/jquery.js'
-			])
-			.pipe(concat('script.concat.js'))
-			.pipe(uglify())
-			.pipe(gulp.dest('tmp'))	
-	}
-	watch([
-		'js/*.js',
-		'!js/*.min.js',
-		'vendor/bootstrap/js/bootstrap.js',
-		'vendor/jquery/jquery.js'
-		], run);
+    function run() {
+        return gulp.src([
+                'js/*.js',
+                '!js/*.min.js',
+                'vendor/bootstrap/js/bootstrap.js',
+                'vendor/jquery/jquery.js'
+            ])
+            .pipe(concat('script.concat.js'))
+            .pipe(uglify())
+            .pipe(gulp.dest('tmp'))
+    }
+    watch([
+        'js/*.js',
+        '!js/*.min.js',
+        'vendor/bootstrap/js/bootstrap.js',
+        'vendor/jquery/jquery.js'
+    ], run);
 
-	return run();
+    return run();
 });
 
-gulp.task('default', ['js:compress'])
+gulp.task('pug', function() {
+    function run() {
+        return gulp.src('pug/*.pug')
+            .pipe(pug())
+            .pipe(gulp.dest('./'))
+    }
+
+    watch('pug/*.pug', run);
+
+    return run();
+});
+
+gulp.task('imagemin', function() {
+    function run() {
+        return gulp.src('bigimage/*')
+            .pipe(imagemin())
+            .pipe(gulp.dest('img'))
+    }
+
+    watch('bigimage/*', run);
+
+    return run();
+});
+
+gulp.task('default', ['js:compress', 'pug', 'imagemin'])
