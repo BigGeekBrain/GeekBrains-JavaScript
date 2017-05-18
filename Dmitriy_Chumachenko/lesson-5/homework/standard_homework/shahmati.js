31da5c59cf8e13e4507a4ea3eb28588413cd62c2
Dmitriy Chumachenko <dm.chumachenko1988@gmail.com>
2017-05-18 17:07:43 +0300

    L5t1

---
+++ Dmitriy_Chumachenko/lesson-5/homework/standard_homework/shahmati.js
@@ -0,0 +1,6 @@
+var container = document.getElementById('container'),
+    black = Array.from(container.getElementsByClassName('inner-black'));
+
+//var black = document.getElementsByClassName('inner-black');
+//black.style.backgroundColor = "black";
+//console.log (black);
